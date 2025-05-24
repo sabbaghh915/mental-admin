8dd26bc2b6457a2b5fc50e7ab8be0fc22fe620a9
sabbaghh915 <128121728+sabbaghh915@users.noreply.github.com>
2025-05-24 16:43:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ManagePages from "./pages/ManagePages";
 import EditSubPage from "./pages/EditSubPage";
